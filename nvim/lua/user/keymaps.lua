61c6ff9c543f7a0d893bde9835e35eab01646f70
Kinzokuzu <sauljcervantes2@gmail.com>
2023-08-26 17:39:56 -0700

    turned files into symbolic links

--- nvim/lua/user/keymaps.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set("n", "<C-E>", ":NvimTreeToggle<cr>") -- toggle Nvim-Tree with control e
-
-vim.keymap.set("i", "{", "{}<Esc>ha") -- auto close brackets
-vim.keymap.set("i", "(", "()<Esc>ha") -- auto close parenthesis 
-vim.keymap.set("i", "[", "[]<Esc>ha") -- auto close square brackets 
-vim.keymap.set("i", "\"", "\"\"<Esc>ha") -- auto close double quotes
-vim.keymap.set("i", "\'", "\'\'<Esc>ha") -- auto close single quotes
