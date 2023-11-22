30aa0cc2a2d4435861840e29a3e1e730fd7e0a2c
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-22 12:14:46 -0800

    old neovim config moved to another branch

--- nvim/lua/user/keymaps.lua
+++
@@ -1,5 +0,0 @@
-vim.keymap.set("n", "<`>", "<Nop>", { noremap = true, silent = true } )
-vim.g.mapleader = "`"
-vim.g.maplocalleader = "`"
-
-vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>") -- toggle Nvim-Tree
