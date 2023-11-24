bf39d8094dbb9889fdc1e759e14714e8c4031fc7
Kinzokuzu <sauljcervantes2@gmail.com>
2023-11-24 04:50:06 -0800

    set up folding

--- init.lua
+++ init.lua
@@ -3,50 +3,51 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- built-in nvim options
-
 local options = {
-  backup = false,                          -- creates a backup file
-  clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
-  cmdheight = 2,                           -- more space in the neovim command line for displaying messages
-  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
-  conceallevel = 0,                        -- so that `` is visible in markdown files
-  fileencoding = "utf-8",                  -- the encoding written to a file
-  hidden = true,                           -- something to do with buffers     
-  hlsearch = true,                         -- highlight all matches on previous search pattern
-  ignorecase = true,                       -- ignore case in search patterns
-  mouse = "a",                             -- allow the mouse to be used in neovim
-  pumheight = 10,                          -- pop up menu height
-  showmode = true,                        -- we don't need to see things like -- INSERT -- anymore
-  showtabline = 2,                         -- always show tabs
-  smartcase = true,                        -- smart case
-  smartindent = true,                      -- make indenting smarter again
-  splitbelow = true,                       -- force all horizontal splits to go below current window
-  splitright = true,                       -- force all vertical splits to go to the right of current window
-  swapfile = false,                        -- creates a swapfile
-  termguicolors = false,                    -- set term gui colors (most terminals support this)
-  timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
-  undofile = true,                         -- enable persistent undo
-  updatetime = 300,                        -- faster completion (4000ms default)
-  writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-  expandtab = true,                        -- convert tabs to spaces
-  shiftwidth = 4,                          -- the number of spaces inserted for each indentation
-  tabstop = 4,                             -- insert 2 spaces for a tab
-  cursorline = true,                       -- highlight the current line
-  number = true,                           -- set numbered lines
-  relativenumber = true,                  -- set relative numbered lines
-  numberwidth = 2,                         -- set number column width to 2 {default 4}
+    backup = false,                          -- creates a backup file
+    clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+    cmdheight = 2,                           -- more space in the neovim command line for displaying messages
+    completeopt = { "menuone", "noselect" }, -- mostly just for cmp
+    conceallevel = 0,                        -- so that `` is visible in markdown files
+    fileencoding = "utf-8",                  -- the encoding written to a file
+    hidden = true,                           -- something to do with buffers     
+    hlsearch = true,                         -- highlight all matches on previous search pattern
+    ignorecase = true,                       -- ignore case in search patterns
+    mouse = "a",                             -- allow the mouse to be used in neovim
+    pumheight = 10,                          -- pop up menu height
+    showmode = true,                        -- we don't need to see things like -- INSERT -- anymore
+    showtabline = 2,                         -- always show tabs
+    smartcase = true,                        -- smart case
+    smartindent = true,                      -- make indenting smarter again
+    splitbelow = true,                       -- force all horizontal splits to go below current window
+    splitright = true,                       -- force all vertical splits to go to the right of current window
+    swapfile = false,                        -- creates a swapfile
+    termguicolors = false,                    -- set term gui colors (most terminals support this)
+    timeoutlen = 300,                        -- time to wait for a mapped sequence to complete (in milliseconds)
+    undofile = true,                         -- enable persistent undo
+    updatetime = 300,                        -- faster completion (4000ms default)
+    writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    expandtab = true,                        -- convert tabs to spaces
+    shiftwidth = 4,                          -- the number of spaces inserted for each indentation
+    tabstop = 4,                             -- insert 2 spaces for a tab
+    cursorline = true,                       -- highlight the current line
+    number = true,                           -- set numbered lines
+    relativenumber = true,                  -- set relative numbered lines
+    numberwidth = 2,                         -- set number column width to 2 {default 4}
 
-  signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
-  wrap = true,                             -- display lines as one long line
-  linebreak = true,                        -- companion to wrap, don't split words
-  scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
-  sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
-  guifont = "monospace:h17",               -- the font used in graphical neovim applications
-  whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
+    foldmethod = "indent",                   -- set folding to follow indent
+    foldnestmax = 1,                         -- set fold nest to only 1 layer
+    signcolumn = "yes",                      -- always show the sign column, otherwise it would shift the text each time
+    wrap = true,                             -- display lines as one long line
+    linebreak = true,                        -- companion to wrap, don't split words
+    scrolloff = 8,                           -- minimal number of screen lines to keep above and below the cursor
+    sidescrolloff = 8,                       -- minimal number of screen columns either side of cursor if wrap is `false`
+    guifont = "monospace:h17",               -- the font used in graphical neovim applications
+    whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 -- vim.opt.shortmess = "ilmnrx"                        -- flags to shorten vim messages, see :help 'shortmess'
@@ -55,19 +56,19 @@ vim.opt.iskeyword:append "-"                           -- hyphenated words recog
 vim.opt.formatoptions:remove({ "c", "r", "o" })        -- don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode.
 vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")  -- separate vim plugins from neovim in case vim still in use
 
--- end built-in options
+-- end most built-in options
 
 -- lazy.nvim setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -77,6 +78,7 @@ vim.g.maplocalleader = " "
 
 -- key remaps go here
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>") -- toggle Nvim-tree
+vim.keymap.set("n", "<leader>t", ":ToggleTerm<cr>") -- toggle Nvim-tree
 
 -- plugins declared here
 local plugins = {
@@ -101,28 +103,29 @@ local plugins = {
 	"saadparwaiz1/cmp_luasnip",
 	"rafamadriz/friendly-snippets",
 
-	"nvim-tree/nvim-tree.lua",
-
-    "lewis6991/gitsigns.nvim",
+    -- other QOL plugins
+	"nvim-tree/nvim-tree.lua", -- file tree
+    "lewis6991/gitsigns.nvim", -- git indicators
+    "akinsho/toggleterm.nvim", -- better terminal inside nvim
 
     -- color schemes
     -- "folke/tokyonight.nvim",
     -- "catppuccin/nvim"
 }
 
-
 -- plugin options
 local opts = {}
 
 -- setup/load plugins
 require("lazy").setup(plugins, opts)
-require("nvim-tree").setup()
 require("nvim-autopairs").setup()
+require("nvim-tree").setup()
 require("gitsigns").setup()
+require("toggleterm").setup({ direction = "float" })
 
 -- colorscheme options
 -- habamax
--- vim.cmd "colorscheme habamax"
+vim.cmd "colorscheme habamax"
 
 -- tokyonight
 -- require("tokyonight").setup({style = "night"})
@@ -131,7 +134,6 @@ require("gitsigns").setup()
 -- catppuccin
 -- require("catppuccin").setup({style = "mocha"})
 -- vim.cmd "colorscheme catppuccin"
-
 
 -- beware, more complicated setups below
 
@@ -143,7 +145,6 @@ require("mason-lspconfig").setup_handlers {
         require("lspconfig")[server_name].setup {}
     end,
 }
-
 
 -- diagnostic options
 vim.diagnostic.config({
@@ -168,7 +169,6 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.s
 
 -- toggle diagnostic window, removes on line change
 vim.keymap.set('n', '<leader>d', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
-
 
 -- autocompletion and snippets setup
 local cmp_status_ok, cmp = pcall(require, "cmp")
