f372ef3b268ac71911cc9f9eeb0c50b2bf6680e5
Saul Cervantes <sauljcervantes2@gmail.com>
2025-08-05 07:31:38 -0700

    New minimalist configuration

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,2 @@
-return {
-	--[[
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			require("monokai-pro").setup({ style = "pro" })
-			vim.cmd.colorscheme "monokai-pro"
-		end
-	}
-	--]]
-	{
-		"navarasu/onedark.nvim",
-		config = function ()
-			require('onedark').setup({
-				style = 'dark'
-			})
-			vim.cmd.colorscheme "onedark"
-		end
-	}
-}
+vim.pack.add({'https://github.com/joshdick/onedark.vim'})
+vim.cmd('colorscheme onedark')
