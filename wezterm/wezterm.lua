b44e57ed0bbc93cccb850f76cc37186994703b1c
Saul Cervantes <sauljcervantes2@gmail.com>
2025-08-13 22:39:23 -0700

    descriptive commit message

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = wezterm.config_builder()
 config.font_size = 13.0
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.color_scheme = 'OneDark (base16)'
+-- config.color_scheme = 'OneDark (base16)'
+config.colors = require('cyberdream')
 
 return config
